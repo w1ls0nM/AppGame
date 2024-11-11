12168d12dae115d057941121944a4840dc5e7c1d
Wilson Miranda <intership.natixis@hotmail.com>
2024-11-11 21:59:34 +0000

    update homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { NavComponent } from "./components/nav/nav.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
