40c6030873c65c7ef76c666ac3e6e69dbc6dcffd
Wilson Miranda <intership.natixis@hotmail.com>
2024-11-17 21:41:16 +0000

    Caso o perfil não tenha uma foto associada, a App deve apresentar um fundo colorido e a primeira letra do nome do utilizador.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,11 +3,12 @@ import { Profile } from '../../Models/profile';
 import { NavComponent } from "../nav/nav.component";
 import { ProfileServiceService } from '../../profile-service.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavComponent],
+  imports: [NavComponent,CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
@@ -16,6 +17,17 @@ export class HeaderComponent {
   profile: Profile | undefined;
 
   defaultAvatar: string = 'https://icon-library.com/images/guest-icon/guest-icon-5.jpg';
+
+  getInitials(name: string | undefined): string {
+    if (!name) {
+      return 'G'; 
+    }
+    const nameParts = name.split(' ');
+    const initials = nameParts.length > 1
+      ? nameParts[0][0] + nameParts[nameParts.length - 1][0] 
+      : nameParts[0][0]; 
+    return initials.toUpperCase();
+  }
 
   constructor(private profileService: ProfileServiceService, private router: Router) {}
 
